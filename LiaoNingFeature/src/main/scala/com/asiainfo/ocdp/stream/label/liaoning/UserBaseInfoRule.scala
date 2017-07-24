03fab44de53b208f9cec086d93324d2c9e253518
yangyuxia <13853186257@139.com>
2017-07-24 16:05:05 +0800

    辽宁标签BUG修改

--- LiaoNingFeature/src/main/scala/com/asiainfo/ocdp/stream/label/liaoning/UserBaseInfoRule.scala
+++ LiaoNingFeature/src/main/scala/com/asiainfo/ocdp/stream/label/liaoning/UserBaseInfoRule.scala
@@ -24,12 +24,16 @@ class UserBaseInfoRule extends Label {
   val codis_key_prefix = "userinfo:" //codis中用户信息查询key的前缀
 
   override def attachLabel(line: Map[String, String], cache: StreamingCache, labelQryData: mutable.Map[String, mutable.Map[String, String]]): (Map[String, String], StreamingCache) = {
-    val imsi_val = line(imsiFiledName)
+//    val imsi_val = line(imsiFiledName)
 
+    var fieldMap =  this.fieldsMap()//初始化labelItems中的标签增强字段
     val info_cols = conf.get(label_props_pname).split(",")
     val qryKeys = getQryKeys(line)
 
-    var fieldMap: Map[String, String] = Map() //定义空map，表示要打的标签字段及值
+    //初始化user_info_cols中的增强字段
+    info_cols.foreach(labelName => {
+      fieldMap += (labelName -> "")
+    })
 
     if (qryKeys.size == 0) {
       // println("-----------qryKeys.size==0 , do nothing!")
@@ -42,51 +46,47 @@ class UserBaseInfoRule extends Label {
       // println("-----------qryKey==" + qryKey + ",userKey==" + userKey)
       //val user_info_map = labelQryData.get(qryKey).get
       val user_info_map = labelQryData.getOrElse(qryKey, Map[String, String]())
-      if (userKey == imsi_val) {
+//      if (userKey == imsi_val) {
         //常规业务的用户标签:由user_info_cols配置，逗号分隔
         info_cols.foreach(labelName => {
-          user_info_map.get(labelName) match {
-            case Some(value) =>
-              fieldMap += (labelName -> value)
-            case None =>
-          }
+          fieldMap += (labelName ->  user_info_map.getOrElse(labelName,""))
         })
-      } else {
-        // do nothing
-      }
-    } else if (qryKeys.size == 2) {
-      //对主叫、被叫都进行信息增强
-      //常规业务用户标签
-      val user_info_map = labelQryData.getOrElse(codis_key_prefix + imsi_val, Map[String, String]())
-
-      info_cols.foreach(labelName => {
-        user_info_map.get(labelName) match {
-          case Some(value) => fieldMap += (labelName -> value)
-          case None =>
-          //发现：现场环境有很多 userinfo:normal_imsi 在redis中没有cache信息，也可能是外地用户，故取消executor日志打印
-          //            logger.debug("= = " * 15 +"in UserBaseInfoRule, got null from labelQryData for key field  = userinfo:" + normal_imsi +" " + labelName)
-        }
-      })
-      qryKeys.foreach(qryKey => {
-        val userKey = qryKey.split(":")(1)
-        // val user_info_map = labelQryData.get(qryKey).get
-        val user_info_map = labelQryData.getOrElse(qryKey, Map[String, String]())
-        //特殊业务的用户标签
-        if (userKey != imsi_val) {
-          //特殊业务的用户标签:在常规业务标签上加前缀
-          info_cols.foreach(labelName => {
-            user_info_map.get(labelName) match {
-              case Some(value) =>
-                fieldMap += (if (userKey == line(calledImsiFileName)) ("called_" + labelName -> value) else ("calling_" + labelName -> value))
-              case None =>
-            }
-          })
-        } else {
-          // do nothing
-        }
-      })
-    } else {
-      // do nothing
+//      } else {
+//        // do nothing
+//      }
+//    } else if (qryKeys.size == 2) {
+//      //对主叫、被叫都进行信息增强
+//      //常规业务用户标签
+//      val user_info_map = labelQryData.getOrElse(codis_key_prefix + imsi_val, Map[String, String]())
+//
+//      info_cols.foreach(labelName => {
+//        user_info_map.get(labelName) match {
+//          case Some(value) => fieldMap += (labelName -> value)
+//          case None =>
+//          //发现：现场环境有很多 userinfo:normal_imsi 在redis中没有cache信息，也可能是外地用户，故取消executor日志打印
+//          //            logger.debug("= = " * 15 +"in UserBaseInfoRule, got null from labelQryData for key field  = userinfo:" + normal_imsi +" " + labelName)
+//        }
+//      })
+//      qryKeys.foreach(qryKey => {
+//        val userKey = qryKey.split(":")(1)
+//        // val user_info_map = labelQryData.get(qryKey).get
+//        val user_info_map = labelQryData.getOrElse(qryKey, Map[String, String]())
+//        //特殊业务的用户标签
+//        if (userKey != imsi_val) {
+//          //特殊业务的用户标签:在常规业务标签上加前缀
+//          info_cols.foreach(labelName => {
+//            user_info_map.get(labelName) match {
+//              case Some(value) =>
+//                fieldMap += (if (userKey == line(calledImsiFileName)) ("called_" + labelName -> value) else ("calling_" + labelName -> value))
+//              case None =>
+//            }
+//          })
+//        } else {
+//          // do nothing
+//        }
+//      })
+//    } else {
+//      // do nothing
     }
 
     //    line.foreach(fieldMap.+(_))
